c1ad536d15e1a1aabbdebbd1ddd68208fc3372d8
Matheus Marciano <83219684+MatheMarte@users.noreply.github.com>
2024-06-19 12:09:45 -0300

    Update ex044.py

--- ex044.py
+++ ex044.py
@@ -1,17 +1,39 @@
-#Exercício Python 44: Elabore um programa que calcule o valor a ser pago por um produto, considerando o seu preço normal e condição de pagamento:
-#– à vista dinheiro/cheque: 10% de desconto
-#– à vista no cartão: 5% de desconto
-#– em até 2x no cartão: preço formal
-#– 3x ou mais no cartão: 20% de juros
+preco = float(input("Valor das compras: R$"))
 
-preco = float(input('Valor das compras:'))
-print('FORMAS DE PAGAMENTO')
-print('[ 1 ] à vista dinheiro/cheque')
-print('[ 2 ] à vista cartão')
-print('[ 3 ] 2x no cartão')
-print('[ 4 ] 3x ou mais no cartão')
-escolha = int(input('Digite sua opção:'))
+print("FORMAS DE PAGAMENTO")
+print("[1] à vista dinheiro/cheque")
+print("[2] à vista cartão")
+print("[3] 2x no cartão")
+print("[4] 3x ou mais no cartão")
+
+escolha = int(input("Digite sua opção: "))
+
 if escolha == 1:
-    print(' O desconto para R${:2f.} é 10%, o valor final é R${:.2f}'.format(preco, (preco-(0.1*preco))))
+    desconto = preco * 0.1
+    valor_final = preco - desconto
+    print(f"O desconto para R${preco:.2f} é de 10%, o valor final é R${valor_final:.2f}")
 elif escolha == 2:
-    print(' O desconto para R${:2f.} é 5%, o valor final é R${:.2f}'.format(preco, (preco-(0.05*preco))))
+    desconto = preco * 0.05
+    valor_final = preco - desconto
+    print(f"O desconto para R${preco:.2f} é de 5%, o valor final é R${valor_final:.2f}")
+elif escolha == 3:
+    valor_parcela = preco / 2
+    print(f"Para o valor do parcelamento escolhido não tem desconto disponível. O valor de cada parcela é R${valor_parcela:.2f}. Sendo o valor final R${preco:.2f}")
+elif escolha == 4:
+    numero_parcelas = int(input("Digite quantas parcelas: "))
+
+    if numero_parcelas < 3:
+        print("Número de parcelas indisponível. Escolha acima de 3x.")
+    else:
+        taxa_juros = 0.2  # Taxa de juros mensal (em decimal, ex: 0.05 para 5%)
+        juros_mensais = preco * taxa_juros
+        valor_parcela = (preco + juros_mensais * numero_parcelas) / numero_parcelas
+        valor_final = preco + (juros_mensais * numero_parcelas)
+
+        print(f"""
+            Sua compra vai ser parcelada em {numero_parcelas}x de R${valor_parcela:.2f} COM JUROS:
+            * Juros mensais: R${juros_mensais:.2f}
+            * Valor final: R${valor_final:.2f}
+            """)
+else:
+    print("Opção inválida. Tente novamente.")
